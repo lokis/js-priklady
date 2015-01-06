6a2f4bc4f7846a893766397393c141a6dca45172
Lokis <lokis.git@gmail.com>
2015-01-06 18:00:58 +0100

    pridany moduly pro should.js

---
+++ Testovani/should-test.js
@@ -0,0 +1,6 @@
+// vyzkousim knihovnu should
+Should(5).be.exactly(5);
+//Should(9).be.exactly(6);
+
+
+(8).should.be.exactly(5, "8 neni 5 ;)");
--- test.js
+++
@@ -1,6 +0,0 @@
-malert(true, 'true');
-malert(34);
-malert('aaa', 'neco');
-malert("bbb");
-
-alert('sss');
